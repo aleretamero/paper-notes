368c170622bf187ce73c047a93f1ef486a4efb47
Alexandre Retamero <alexandreretamero@gmail.com>
2023-09-21 15:36:36 -0300

    feat: add new styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,6 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
+import { flexStyles } from './mixins/flexStyles';
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary-light: #694ED8;
+    --primary-dark: #6600CC;
+    --dark: #101010;
+    --light: #ddd;
+    --white: #fff;
+    --black: #000;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -8,9 +18,15 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: Ubuntu, sans-serif;
+    font-family: 'Roboto Flex', sans-serif;;
     background-color: ${(props) => props.theme.colors.background};
     color: ${(props) => props.theme.colors.text};
+  }
+
+  #root {
+    ${flexStyles({ justify: 'space-between', align: 'stretch' })}
+    width: 100%;
+    min-height: 100vh;
   }
 
   .auto-padding {
