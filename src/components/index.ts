4ea1f31b32842f360938332e7c558851fc91e46c
Alexandre Retamero <alexandreretamero@gmail.com>
2023-09-21 15:47:17 -0300

    feat: add exports components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { Button } from './Button';
+export { ButtonLink } from './ButtonLink';
+export { Form } from './Form';
+export { Header } from './Header';
+export { Image } from './Image';
+export { Input } from './Input';
+export { Title } from './Title';
