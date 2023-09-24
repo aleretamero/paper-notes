83bc28b3999861da50a571502b1b42cba997cd36
Alexandre Retamero <alexandreretamero@gmail.com>
2023-09-23 22:14:11 -0300

    change url

--- src/services/api/Api.ts
+++ src/services/api/Api.ts
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
-export const Api = axios.create({ baseURL: 'http://localhost:3333' });
+export const Api = axios.create({
+  baseURL: 'https://api-paper-notes.vercel.app',
+});
